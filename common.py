acd6950b1f20d163810931164f18361558615a89
YaodongLiu <ttttttliu@qq.com>
2021-04-15 22:03:27 +0800

    common variable
    common variable
    针对本项目定义的通常变量

---
+++ common.py
@@ -0,0 +1,26 @@
+APP_DESCRIPTION = "-" * 20 + "My Py-DNN by Pytorch" + "-" * 20 + "\n"
+
+IMG_EXTENSIONS = [
+    '.jpg', '.JPG', '.jpeg', '.JPEG',
+    '.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP',
+    '.tif', '.TIF', '.tiff', '.TIFF',
+]
+
+TRAIN = "train"
+VALID = "valid"
+TEST = "test"
+
+CNN_MODELS = [
+    'VGG',
+    'ResNet',
+    'DenseNet',
+    'ResNext',
+]
+
+OPTIMIZERS = [
+    'Adam',
+]
+
+LOSS_FUNCTIONS = [
+    'NLLLoss',
+]
