06821727af69fa81d3093d606422c5ce86666fb2
YaodongLiu <ttttttliu@qq.com>
2021-04-27 23:40:11 +0800

    重新梳理结构

--- model/models_opt.py
+++
@@ -1,9 +0,0 @@
-from model.image_classify_model import PreCNNModel
-
-MODELS_OPT = {
-    'image': PreCNNModel,
-}
-
-
-def set_model(model_type):
-    return MODELS_OPT[model_type]
---
+++ model/save.py
@@ -0,0 +1,28 @@
+import torch
+
+
+class ModelSave:
+    def __init__(self, opt_):
+        self.valid_best_loss = float('inf')
+        self.last_better_iter = 0
+        self.path = opt_.model_save
+        self.state_dict = None
+
+    def save_model_state(self, loss_, model_, iter_):
+        if loss_ < self.valid_best_loss:
+            self.valid_best_loss = loss_
+            self.state_dict  = model_.state_dict()
+            self.last_better_iter = iter_
+
+    def is_shut_down(self, iter_):
+        max_iter = 2000
+        if iter_ - self.last_better_iter > max_iter:
+            print("No optimization for a long time({} batches), auto-stopping...".format(max_iter))
+            return True
+        else:
+            return False
+
+    def save_model(self, model_):
+        model_.load_state_dict(self.state_dict)
+        torch.save(model_, self.path)
+        torch.save(self.state_dict,self.path+".state_dict")
