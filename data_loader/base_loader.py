60267916cd4718b36448f5373cc14988dcf70e2b
YaodongLiu <ttttttliu@qq.com>
2021-05-03 00:00:30 +0800

    抽象出BaseLoader（基本数据加载）

---
+++ data_loader/base_loader.py
@@ -0,0 +1,101 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
+
+from common import TRAIN, VALID, TEST
+
+
+class BaseDataset(Dataset):
+    def __init__(self):
+        super(BaseDataset, self).__init__()
+        self.classes = []
+        self.inputs = []
+        self.labels = []
+
+    def __getitem__(self, item):
+        return self.inputs[item], self.labels[item]
+
+    def __len__(self):
+        return len(self.inputs)
+
+
+class DataIter:
+    def __init__(self, dataloader, device):
+        self.dataloader = [(x, y) for x, y in tqdm(dataloader)]
+        self.index = 0
+        self.len = len(self.dataloader)
+        self.device = device
+
+    def _to_tensor(self, x, y):
+        return x.to(self.device), y.to(self.device)
+
+    def __next__(self):
+        if self.index >= self.len:
+            self.index = 0
+            raise StopIteration
+        else:
+            x, y = self.dataloader[self.index]
+            self.index = self.index + 1
+            x, y = self._to_tensor(x, y)
+            return x, y
+
+    def __iter__(self):
+        return self
+
+    def __len__(self):
+        return self.len
+
+
+class BaseLoader:
+    def __init__(self, opt):
+        self.data_dir = opt.data
+        self.batch_size = opt.batch_size
+        self.train_loaded = None
+        self.class_nums = 0
+        self.valid_loaded = None
+        self.test_loaded = None
+        self.thread = opt.thread_num
+        self.gpu_ids = opt.gpu_ids
+        self.device = self._set_device()
+        self.inputs = {}
+        self.is_train = (opt.test is False)
+
+    def _load_sets(self, tag=TRAIN, **kwargs):
+        data_sets = BaseDataset()
+        return data_sets
+
+    def _load_train_valid(self, **kwargs):
+        train_sets = self._load_sets(tag=TRAIN, **kwargs)
+        self.train_loaded = DataLoader(train_sets, batch_size=self.batch_size, shuffle=True,
+                                       num_workers=self.thread)
+        valid_sets = self._load_sets(tag=VALID, **kwargs)
+        self.valid_loaded = DataLoader(valid_sets, batch_size=self.batch_size, shuffle=True,
+                                       num_workers=self.thread)
+        self.class_nums = len(train_sets.classes)
+        print("Train sets:", len(train_sets))
+        print("Valid sets:", len(valid_sets))
+        print("Batch size:", self.batch_size)
+        print("Class nums:", self.class_nums)
+
+    def _load_test(self, **kwargs):
+        test_sets = self._load_sets(tag=TEST, **kwargs)
+        self.test_loaded = DataLoader(test_sets, batch_size=self.batch_size, num_workers=self.thread)
+
+    def init_(self, **kwargs):
+        if self.is_train:
+            self._load_train_valid(**kwargs)
+            self.inputs[TRAIN] = DataIter(self.train_loaded, self.device)
+            self.inputs[VALID] = DataIter(self.valid_loaded, self.device)
+        else:
+            self._load_test(**kwargs)
+            self.inputs[TEST] = DataIter(self.test_loaded, self.device)
+
+    def _set_device(self):
+        device = "cpu"
+        cuda = torch.cuda.is_available()
+        if self.gpu_ids[0] != -1:
+            if cuda:
+                device = torch.device('cuda:{}'.format(self.gpu_ids[0]))
+            else:
+                device = torch.device('cpu')
+        return device
--- data_loader/image_loader.py
+++ data_loader/image_loader.py
@@ -1,11 +1,9 @@
 import os
 
 import numpy as np
-import torch
 from torchvision import transforms, datasets
-from torch.utils.data import DataLoader
-from tqdm import tqdm
-from common import TRAIN, VALID, TEST
+from common import TRAIN
+from data_loader.base_loader import BaseLoader
 
 '''
 使用pytorch中的torchvision.datasets模块，可以很方便的处理输入数据
@@ -47,96 +45,26 @@ def this2image(tensor):
     return image
 
 
-class DataIter:
-    def __init__(self, dataloader, device):
-        self.dataloader = [(x, y) for x, y in tqdm(dataloader)]
-        self.index = 0
-        self.len = len(self.dataloader)
-        self.device = device
-
-    def _to_tensor(self, x, y):
-        return x.to(self.device), y.to(self.device)
-
-    def __next__(self):
-        if self.index >= self.len:
-            self.index = 0
-            raise StopIteration
-        else:
-            x, y = self.dataloader[self.index]
-            self.index = self.index + 1
-            x, y  = self._to_tensor(x, y)
-            return x, y
-
-    def __iter__(self):
-        return self
-
-    def __len__(self):
-        return self.len
-
-
-class ImageLoader():
+class ImageLoader(BaseLoader):
     def __init__(self, opt):
-        self.data_dir = opt.data
-        self.batch_size = opt.batch_size
-        self.train_loaded = None
-        self.class_nums = 0
-        self.valid_loaded = None
-        self.test_loaded = None
-        self.thread = opt.thread_num
-        self.gpu_ids = opt.gpu_ids
-        self.device = self._set_device()
-        self.inputs = {}
-        self.is_train = (opt.test is False)
+        super(ImageLoader, self).__init__(opt)
 
-    def _load_train_valid(self, input_size=224):
-        if not os.path.exists(self.data_dir):
-            print("Missing data folders")
+    def _load_sets(self, tag=TRAIN, **kwargs):
+        if kwargs.get("input_size") is None:
+            print("Missing data input size. ")
             exit(-1)
-        train_data_dir = os.path.join(self.data_dir, TRAIN)
-        valid_data_dir = os.path.join(self.data_dir, VALID)
-        if not os.path.exists(train_data_dir) or not os.path.exists(valid_data_dir):
-            print("Missing {} and {} folders".format(TRAIN, VALID))
-            exit(-1)
-        train_transforms = get_transforms(True, input_size)
-        train_sets = datasets.ImageFolder(train_data_dir, train_transforms)
-        self.train_loaded = DataLoader(train_sets, batch_size=self.batch_size, shuffle=True,
-                                       num_workers=self.thread)
-        valid_transforms = get_transforms(False, input_size)
-        valid_sets = datasets.ImageFolder(valid_data_dir, valid_transforms)
-        self.valid_loaded = DataLoader(valid_sets, batch_size=self.batch_size, shuffle=True,
-                                       num_workers=self.thread)
-        self.class_nums = len(train_sets.classes)
-        print("Train sets:", len(train_sets))
-        print("Valid sets:", len(valid_sets))
-        print("Batch size:", self.batch_size)
-        print("Class nums:", self.class_nums)
-
-    def _load_test(self, input_size=224):
-        if not os.path.exists(self.data_dir):
-            print("Missing data folders")
-            exit(-1)
-        test_data_dir = os.path.join(self.data_dir, TEST)
-        if not os.path.exists(test_data_dir):
-            print("Missing {} folders".format(TEST))
-            exit(-1)
-        test_sets = datasets.ImageFolder(test_data_dir, get_transforms(False, input_size))
-        self.test_loaded = DataLoader(test_sets, batch_size=self.batch_size, num_workers=self.thread)
+        else:
+            input_size = kwargs.get("input_size")
+            if not os.path.exists(self.data_dir):
+                print("Missing data folders")
+                exit(-1)
+            data_dir = os.path.join(self.data_dir, tag)
+            if not os.path.exists(data_dir):
+                print("Missing {} folders".format(tag))
+                exit(-1)
+            data_transforms = get_transforms(tag == TRAIN, input_size)
+            data_sets = datasets.ImageFolder(data_dir, data_transforms)
+            return data_sets
 
     def init_(self, input_size=224):
-        if self.is_train:
-            self._load_train_valid(input_size)
-            self.inputs[TRAIN] = DataIter(self.train_loaded, self.device)
-            self.inputs[VALID] = DataIter(self.valid_loaded, self.device)
-        else:
-            self._load_test(input_size)
-            self.inputs[TEST] = DataIter(self.test_loaded, self.device)
-
-    def _set_device(self):
-        device = "cpu"
-        cuda = torch.cuda.is_available()
-        if self.gpu_ids[0] != -1:
-            if cuda:
-                device = torch.device('cuda:{}'.format(self.gpu_ids[0]))
-            else:
-                device = torch.device('cpu')
-        return device
+        BaseLoader.init_(self, input_size=input_size)
