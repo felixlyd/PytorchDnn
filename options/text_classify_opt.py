2cf3a2c7aca0caf6a68a181cb98c616a53873ac2
YaodongLiu <ttttttliu@qq.com>
2021-05-03 12:23:29 +0800

    添加文本分类的命令行参数处理

---
+++ options/text_classify_opt.py
@@ -0,0 +1,26 @@
+from common import TEXT_MODELS, EMBEDDINGS
+from options.base_opt import BaseOpt
+
+
+class TextClassifyOpt(BaseOpt):
+    def __init__(self):
+        super(TextClassifyOpt, self).__init__()
+
+    @staticmethod
+    def add_model_args(parser):
+        parser.add_argument('--model', type=str, default='TextRNN', choices=TEXT_MODELS,
+                            help='choose which model to use. ')
+        parser.add_argument('--embedding', type=str, default='Sogou', choices=EMBEDDINGS,
+                            help="choose embeddings.")
+        parser.add_argument('--pad_size', type=int, default=32, help="padding size.")
+        parser.add_argument('--word', default=False, type=bool, help='True for word, False for char')
+        BaseOpt.add_model_args(parser)
+
+    @staticmethod
+    def add_resource_args(parser):
+        parser.add_argument('--data', default='resources/data',
+                            help="for train, the path should have train.txt and valid.txt;for test,"
+                                 " should have test.txt. ")
+        parser.add_argument('--out', help="out folders for test")
+        parser.add_argument('--vocab', help='vocab dict path. ')
+        BaseOpt.add_resource_args(parser)
